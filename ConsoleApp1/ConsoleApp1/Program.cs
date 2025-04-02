bb2aa205e3d1830d4ffffc12bf912f708c70b490
Hollow4U <nanishinde123@gmail.com>
2025-04-02 05:34:59 -0500

    Herencia

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,16 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        public static  List<Characters> character = new List<Characters>();       
+       
+
         static void Main(string[] args)
         {
+            character.Add(new Characters("David",20,3,2));
+            character.Add(new Characters("Victor",10, 5, 8));
+
+            character[1].attack(0);
         }
+        
     }
 }
